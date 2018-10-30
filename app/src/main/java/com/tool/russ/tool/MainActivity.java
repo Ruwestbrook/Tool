795e48ea126b0793ada73b8f81bc8ddb5631dc28
russell <1406409859@qq.com>
2018-10-31 00:25:59 +0800

    下拉刷新

--- app/src/main/java/com/tool/russ/tool/MainActivity.java
+++ app/src/main/java/com/tool/russ/tool/MainActivity.java
@@ -9,24 +9,38 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.tool.russ.view.RefreshView;
 import com.tool.russ.view.TxView;
 
 public class MainActivity extends AppCompatActivity {
-    private boolean flag=false;
-    TxView view;
+    private RecyclerView mRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        view=findViewById(R.id.all);
-        view.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView=findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
             @Override
-            public void onClick(View v) {
-                flag=!flag;
-                view.setSelected(flag);
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new RecyclerView.ViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.item,parent,false)) {};
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                TextView textView=holder.itemView.findViewById(R.id.text);
+                textView.setText("这是第"+(position+1)+"个Item");
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
             }
         });
+
     }
 }
--- view/src/main/java/com/tool/russ/view/RefreshView.java
+++ view/src/main/java/com/tool/russ/view/RefreshView.java
@@ -1,57 +1,83 @@
 package com.tool.russ.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class RefreshView extends RecyclerView {
+public class RefreshView extends LinearLayout implements View.OnTouchListener {
+    private static final String TAG = "RefreshView";
+    private View headView;
+    private RecyclerView mRecyclerView;
+    LinearLayout.LayoutParams layoutParams;
     public RefreshView(Context context) {
-        super(context);
+        this(context,null);
     }
+    private boolean isFirst=false;
 
     public RefreshView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs,0);
     }
 
     public RefreshView(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setOrientation(LinearLayout.VERTICAL);
+        setLongClickable(true);
+        headView=LayoutInflater.from(context).inflate(R.layout.refresh_title,this,false);
+        addView(headView,0);
     }
 
-    @Override
-    protected void onMeasure(int widthSpec, int heightSpec) {
 
-        ViewGroup.MarginLayoutParams layoutParams= (MarginLayoutParams) getLayoutParams();
-        layoutParams.topMargin=-150;
-        setLayoutParams(layoutParams);
-        super.onMeasure(widthSpec, heightSpec);
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        if(changed && !isFirst){
+            isFirst=true;
+            int height=headView.getMeasuredHeight();
+             layoutParams= (LayoutParams) headView.getLayoutParams();
+            layoutParams.topMargin=-height;
+            headView.setLayoutParams(layoutParams);
+            mRecyclerView= (RecyclerView) getChildAt(1);
+            mRecyclerView.setOnTouchListener(this);
+        }
+
     }
 
     private float downY=0;
+
+
+
     @Override
-    public boolean onTouchEvent(MotionEvent e) {
-        switch (e.getAction()){
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        Log.d(TAG, "onTouch: "+headView.getLayoutParams().getClass());
+        Log.d(TAG, "onTouch: ");
+        switch (motionEvent.getAction()){
             case MotionEvent.ACTION_UP:
                 //抬起
-                ViewGroup.MarginLayoutParams layoutParams1= (MarginLayoutParams) getLayoutParams();
-                layoutParams1.topMargin= -150;
-                setLayoutParams(layoutParams1);
+                downY=0;
+                layoutParams.height=0;
+                headView.setLayoutParams(layoutParams);
                 break;
             case MotionEvent.ACTION_DOWN:
                 //按下
-                downY=e.getY();
+                downY=motionEvent.getRawY();
                 break;
             case MotionEvent.ACTION_MOVE:
                 //移动
-                float y=e.getY();
-                ViewGroup.MarginLayoutParams layoutParams= (MarginLayoutParams) getLayoutParams();
-                layoutParams.topMargin= (int) (-150+y-downY);
-                setLayoutParams(layoutParams);
+                float y=motionEvent.getRawY();
+
+               layoutParams.height= (int) (y-downY);
+                headView. setLayoutParams(layoutParams);
         }
-
-
-        return super.onTouchEvent(e);
+        return false;
     }
 }
