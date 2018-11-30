ad2d6c0eddd5492e1f67d4f664f8a4514eb14704
russ <1406409859@qq.com>
2018-11-30 19:18:26 +0800

    0.0

--- view/src/main/java/com/tool/russ/view/CircleView.java
+++ view/src/main/java/com/tool/russ/view/CircleView.java
@@ -2,18 +2,25 @@ package com.tool.russ.view;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Shader;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 
 public class CircleView extends android.support.v7.widget.AppCompatImageView {
+    private static final String TAG = "CircleView";
+    private int width;
+    private int height;
+    private int type=0;
+    private int radius;
     public CircleView(Context context) {
         this(context,null);
     }
@@ -22,9 +29,40 @@ public class CircleView extends android.support.v7.widget.AppCompatImageView {
         this(context, attrs,0);
     }
 
+    /**
+     * 相同点：返回获取某个dimen的值，如果dimen单位是dp或sp，则需要将其乘以density（屏幕密度）；如果单位是px，则不用。
+     *
+     * 不同点：
+     * getDimension：返回类型为float，
+     * getDimensionPixelSize：返回类型为int，由浮点型转成整型时，采用四舍五入原则。 
+     * getDimensionPixelOffset：返回类型为int，由浮点型转成整型时，原则是忽略小数点部分。
+     */
+
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        @SuppressLint("Recycle")
+        TypedArray array=context.obtainStyledAttributes(attrs,R.styleable.CircleView);
+        type=array.getInt(R.styleable.CircleView_circleType,0);
+        radius=array.getDimensionPixelSize(R.styleable.CircleView_circleRadius,20);
+        array.recycle();
     }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width=getMeasuredWidth();
+        height=getMeasuredHeight();
+        Log.d(TAG, "onMeasure: width="+width);
+        Log.d(TAG, "onMeasure: height="+height);
+        if(type!=2){
+            int x=Math.min(width,height);
+            width=x;
+            height=x;
+            setMeasuredDimension(width,height);
+        }
+    }
+
     @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
@@ -33,10 +71,16 @@ public class CircleView extends android.support.v7.widget.AppCompatImageView {
         Paint mPaint=new Paint();
         BitmapShader shader=new BitmapShader(bitmap,BitmapShader.TileMode.CLAMP,BitmapShader.TileMode.CLAMP);
         Matrix mMatrix = new Matrix();
-        mMatrix.setScale(0.4f, 0.4f);
+        int w=bitmap.getWidth();
+        int h=bitmap.getHeight();
+        mMatrix.setScale(width>w?w*1.0f/width:width*1.0f/w, height>h?h*1.0f/height:height*1.0f/h);
         shader.setLocalMatrix(mMatrix);
         mPaint.setShader(shader);
-        canvas.drawCircle(getWidth()/2, getWidth()/2, getWidth()/2, mPaint);
+        if(type==2){
+            canvas.drawRoundRect(new RectF(0,0,width,height),radius,radius,mPaint);
+        }else {
+             canvas.drawCircle(getWidth()/2, getWidth()/2, getWidth()/2, mPaint);
+        }
     }
 
     private Bitmap getBitmap() {
