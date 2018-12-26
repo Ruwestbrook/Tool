3f98fe3a998a58d32cdc87838a0b9667c1653a70
russell <1406409859@qq.com>
2018-12-26 20:25:13 +0800

    画图呀画图

--- app/src/main/java/com/tool/russ/tool/MainActivity.java
+++ app/src/main/java/com/tool/russ/tool/MainActivity.java
@@ -1,23 +1,8 @@
 package com.tool.russ.tool;
 
 import android.annotation.SuppressLint;
-import android.os.Handler;
-import android.os.Message;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.tool.russ.view.RefreshListener;
-import com.tool.russ.view.RefreshView;
-import com.tool.russ.view.TxView;
-
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("HandlerLeak")
