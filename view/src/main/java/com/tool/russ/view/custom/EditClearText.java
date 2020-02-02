4f4c257b5ac47038fb6db36f86854e28e9ba89e6
russell <1406409859@qq.com>
2020-02-03 00:10:27 +0800

    带删除功能的EditText完成,可以自定义设置关闭按钮颜色,drawable用的不是png,而是canvas绘制而成

--- view/src/main/java/com/tool/russ/view/custom/EditClearText.java
+++ view/src/main/java/com/tool/russ/view/custom/EditClearText.java
@@ -1,6 +1,27 @@
 package com.tool.russ.view.custom;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import com.tool.russ.view.R;
 
 /**
  * author: russell
@@ -8,8 +29,12 @@ import android.util.AttributeSet;
  * describe：自定义的EditText 可以取消输入
  */
 public class EditClearText extends android.support.v7.widget.AppCompatEditText {
-    private int width;
-    private int height;
+;
+    int closeSize;
+    Drawable drawable;
+
+    int closeColor;
+
     public EditClearText(Context context) {
         this(context,null);
     }
@@ -19,8 +44,171 @@ public class EditClearText extends android.support.v7.widget.AppCompatEditText {
     }
 
     public EditClearText(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        super(context, attrs, android.R.attr.editTextStyle);
+
+        TypedArray array=context.obtainStyledAttributes(attrs,R.styleable.EditClearText);
+
+        closeColor=array.getColor(R.styleable.EditClearText_closeColor,Color.parseColor("#DFDFDF"));
+
+        array.recycle();
+
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+                setClose(s.length()>0);
+
+            }
+        });
+
+
+        setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    setClose(getText().length()>0);
+                }else {
+                    setClose(false);
+                }
+
+            }
+        });
+
+
     }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        float x=0.8f;
+
+        if (getBackground() instanceof ColorDrawable) {
+          x=0.5f;
+        }
+
+        closeSize= (int) ((MeasureSpec.getSize(heightMeasureSpec)-getPaddingBottom()-getPaddingTop())*x);
+
+
+        setRightClose();
+    }
+
+    void setRightClose(){
+
+       Bitmap bitmap=Bitmap.createBitmap(closeSize,closeSize, Bitmap.Config.ARGB_8888);
+       Canvas canvas=new Canvas(bitmap);
+       Paint paint=new Paint();
+       paint.setColor(closeColor);
+
+
+       int circle=closeSize/2;
+
+
+       canvas.drawCircle(circle,circle,circle,paint);
+
+
+
+       canvas.rotate(45,circle,circle);
+
+
+
+
+
+
+       if (getBackground() instanceof ColorDrawable) {
+           ColorDrawable colorDrawable = (ColorDrawable) getBackground();
+           int color = colorDrawable.getColor();
+           paint.setColor(color);
+       }else {
+           paint.setColor(Color.WHITE);
+       }
+
+
+
+
+
+
+       int closeWidth=closeSize/2;
+       int closeHeight=closeSize/8;
+
+
+
+       Rect rect;
+
+       rect=new Rect(circle-closeWidth/2,circle-closeHeight/2,
+               circle+closeWidth/2,circle+closeHeight/2);
+
+       canvas.drawRect(rect,paint);
+
+
+       rect=new Rect(circle-closeHeight/2,circle-closeWidth/2,
+               circle+closeHeight/2,circle+closeWidth/2);
+
+       canvas.drawRect(rect,paint);
+
+
+
+
+       drawable=new BitmapDrawable(bitmap);
+
+
+
+       drawable.setBounds(0,0,closeSize,closeSize);
+
+
+       //setCompoundDrawables(null,null,drawable, null);
+
+    }
+
+
+    private static final String TAG = "EditClearText";
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+
+        if(event.getAction()==MotionEvent.ACTION_UP){
+            int x= (int) event.getX();
+
+
+
+            int left=getMeasuredWidth()-closeSize-getPaddingRight();
+            int right=left+closeSize;
+
+            if(x>left && x<right ){
+                setText("");
+                return  true;
+            }
+
+
+        }
+
+
+        return super.onTouchEvent(event);
+    }
+
+
+
+    private void  setClose(boolean flag){
+        if(flag){
+            setCompoundDrawables(null,null,drawable, null);
+        }else {
+
+            setCompoundDrawables(null,null,null, null);
+        }
+    }
+
 
 
 }
